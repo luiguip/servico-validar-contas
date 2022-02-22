475ff3e25c6953670b04ce56f789cbc801cf686b
Luis Guilherme de A <luisguilhermepdea@gmail.com>
2022-02-22 10:09:54 -0300

    Refatoração: Limpeza imports ReceitaServiceTest

--- src/test/java/tech/luigui/workers_management/service/ReceitaServiceTest.java
+++ src/test/java/tech/luigui/workers_management/service/ReceitaServiceTest.java
@@ -1,14 +1,11 @@
 package tech.luigui.workers_management.service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import tech.luigui.workers_management.service.ReceitaService;
 
 @SpringBootTest
 public class ReceitaServiceTest {
